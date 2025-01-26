82dfc85b44882344d39c07f0e635e29db4728c48
johannes-vdm <johannes@webit.net>
2025-01-26 22:08:24 +0100

    Add Vercel Analytics for tracking and insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 import { ThemeProvider } from "./providers"
 import type { ReactNode } from "react"
+import { Analytics } from "@vercel/analytics/react"
+
 
 export const metadata: Metadata = {
   title: 'WEBIT, Strategic Innovation Partners',
@@ -22,6 +24,7 @@ export default function RootLayout({
         <ThemeProvider>
           {children}
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   )
